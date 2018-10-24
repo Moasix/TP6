8500c75258f4e6c3f8037c136755d00945285381
BLANQUET Antoine <blanquan@hostname.iut2.upmf-grenoble.fr>
2018-10-24 09:13:45 +0200

    gzghr

---
+++ view/header.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Header wsh wsh</title>
+  </head>
+  <body>
+
+    <header>
+      <div>
+        <a href="#">  <img src="../view/images/adibou_master_race.gif" alt="Logo de PC Master Race"></a>
+        <input type="text" name="search" value="rechercher">
+        <a href="#">  <img src="../view/images/panier.png" alt="Panier Utilisateur"></a>
+
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">QUI SOMMES NOUS ?</a></li>
+          <li><a href="#">LE TOP 10</a></li>
+          <li><a href="#">GENRES</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+      </nav>
+
+    </header>
+
+  </body>
+</html>
---
+++ view/main_page.view.php
@@ -0,0 +1,20 @@
+<html lang="fr" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title>Juk Zik</title>
+      <link rel="stylesheet" type="text/css" href="styleMusique.css" />
+    </head>
+    <body>
+
+
+      <header>
+
+      </header>
+
+
+      <h1>Jukebox</h1>
+
+
+
+    </body>
+</html>
