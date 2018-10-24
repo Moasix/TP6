fb8ca928687184450cf5589dae45581ef03e04ad
SULMONT Manuel <sulmontm@hostname.iut2.upmf-grenoble.fr>
2018-10-24 15:51:57 +0200

    dvg

--- view/main_page.view.php
+++ view/main_page.view.php
@@ -1,20 +1,14 @@
 <html lang="fr" dir="ltr">
     <head>
       <meta charset="utf-8">
-      <title>Juk Zik</title>
+      <title>PC Master Race</title>
       <link rel="stylesheet" type="text/css" href="styleMusique.css" />
     </head>
     <body>
-
-
-      <header>
-
-      </header>
-
-
-      <h1>Jukebox</h1>
-
-
-
+      <?php foreach ($games as $key => $value) { ?>
+        <a href="../jeu.ctrl.php/?ind=<?=$value->ref?>">
+          <img src="../data/covers/<?=$value->image?>" alt="">
+        </a>
+      <?php } ?>
     </body>
 </html>
