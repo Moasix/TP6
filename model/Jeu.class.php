38bc6e2ca442d7fb9b71cb923d70d93b0929ecc6
SULMONT Manuel <sulmontm@hostname.iut2.upmf-grenoble.fr>
2018-10-24 08:23:47 +0200

    zaz

--- model/Jeu.class.php
+++ model/Jeu.class.php
@@ -1,8 +1,8 @@
 <?php
     // Un article en vente
-    class Article {
+    class Jeu {
         public $ref;       // Référence unique
-        public $libelle;   // Nom de l'article
+        public $titre;   // Nom de l'article
         public $categorie; // identifiant de catégorie
         public $prix;      // le prix
         public $image;     // Nom du fichier image
